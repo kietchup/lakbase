8bb711ff45ec2527fbac2f92d031f91fc5c24577
haduken2 <tiroadto@gmail.com>
2025-10-15 10:25:51 +0800

    initial commit
    commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return <h1>Hello React + Laravel 🚀</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
